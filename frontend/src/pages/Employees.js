6d888a8739c298f05497aaec56a3db294c04b0c3
Yogeshwar Murugesan <yogeshwarmurugesan4@gmail.com>
2024-10-23 18:26:55 +0530

    Updated all files including AddEmployee form, backend, and packages

--- frontend/src/pages/Employees.js
+++ frontend/src/pages/Employees.js
@@ -1,59 +1,66 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom';
+
 
 const Employees = () => {
 
     const [employees, setEmployee] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         axios.get('http://localhost:3001/Employees')
-        .then((res)=>{
-            setEmployee(res.data)
-        })
-        .catch((err)=>{
-            console.log(err);
-            
-        })
-    },[])
+            .then((res) => {
+                setEmployee(res.data)
+            })
+            .catch((err) => {
+                console.log(err);
 
-  return (
-    <div className='container'>
-        <h1 className='text-center'>Employees Details</h1>
-        <table className='table table-striped'>
-            <thead>
-                <tr>
-                    <th>Sr.No</th>
-                    <th>Emp. Id</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    <th>Department</th>
-                    <th>Work Location</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
+            })
+    }, [])
 
-            <tbody>
-                {employees.map((employee, index)=>(
-                    <tr key={index}>
-                    <td>{index+1}</td>
-                    <td>{employee.empid}</td>
-                    <td>{employee.name}</td>
-                    <td>{employee.email}</td>
-                    <td>{employee.jobTitle}</td>
-                    <td>{employee.department}</td>
-                    <td>{employee.workLocation}</td>
-                    <td>
-                        <button className='btn btn-success me-2'>Update</button>
-                        <button className='btn btn-danger'>Delete</button>
-                    </td>
-                </tr>
-                ))}
-                
-            </tbody>
-        </table>
-    </div>
-  )
+    return (
+        <div classNameName='container'>
+            <h1 classNameName='text-center'>Employees Details</h1>
+            <h2 className='text-center mb-5'>{employees.length} Employees</h2>
+            <div className="container">
+                <div className="row">
+                    {employees.map((employee, index) => (
+                        <div className="col col-lg-3 col-md-6 col-sm-12 mb-4">
+                            <div className="card" style={{ width: '20rem' }} key={index}>
+                                <div className="card-body">
+                                    <h5 className="card-title">{employee.name}</h5>
+                                    <h6 className="card-subtitle mb-2 text-body-secondary">{employee.jobTitle}</h6>
+                                    <div className="row mt-4">
+                                        <div className="col col-6 ">
+                                            <p className="card-text">Department: <br />{employee.department}</p>
+                                        </div>
+
+                                        <div className="col col-6">
+                                            <p className="card-text">Hired Date: <br />{employee.doj}</p>
+                                        </div>
+                                    </div>
+                                    <div className="row my-3">
+                                        <div className="col col-6">
+                                            <Link to={`/employee/${employee.id}`} className="card-link">{employee.email}</Link>
+
+                                        </div>
+                                        <div className="col col-6">
+                                            <Link to={`/employee/${employee.id}`} className="card-link">{employee.phNo}</Link>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    ))}
+                </div>
+
+            </div>
+        </div>
+
+    )
 }
 
 export default Employees
